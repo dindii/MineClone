6549257cde3beadb9bdef3e05418645dd501fe1e
didimojuni <didimoribeirojunior@hotmail.com>
2020-05-15 03:41:38 -0300

    Added comment

--- MineCloneProject/src/MCP/Event/ApplicationEvent.h
+++ MineCloneProject/src/MCP/Event/ApplicationEvent.h
@@ -8,11 +8,19 @@ namespace MC
 	class WindowResizeEvent : public Event
 	{
 	public:
+		/**
+		* @brief Constructor which takes two uint32_t.
+		* @param width  - uint32_t to store the new width value after  resize.
+		* @param height - uint32_t to store the new height value after resize.
+		*/
 		WindowResizeEvent(unsigned int width, unsigned int height) : m_Width(width), m_Height(height) {};
 
 		inline unsigned int GetWidth() const { return m_Width; }
 		inline unsigned int GetHeight() const { return m_Height; }
 
+		/**
+		* @brief Return a string with the resize event name and the new width and height.
+		*/
 		std::string ToString() const override
 		{
 			std::stringstream ss;
@@ -20,6 +28,9 @@ namespace MC
 			return ss.str();
 		}
 
+		/**
+		* @brief Setup the WindowResize event type and category in order to have some "introspection".
+		*/
 		EVENT_CLASS_TYPE(WindowResize)
 		EVENT_CLASS_CATEGORY(EventCategoryApplication)
 
@@ -30,7 +41,15 @@ namespace MC
 	class WindowCloseEvent : public Event
 	{
 	public:
+
+		/**
+		* @brief Constructor
+		*/
 		WindowCloseEvent() {};
+
+		/**
+		* @brief Setup the WindowClose event type and category in order to have some "introspection".
+		*/
 		EVENT_CLASS_TYPE(WindowClose)
 		EVENT_CLASS_CATEGORY(EventCategoryApplication)
 	};
@@ -38,14 +57,28 @@ namespace MC
 	class AppTickEvent : public Event
 	{
 	public:
+		/**
+		* @brief Constructor.
+		*/
 		AppTickEvent() {};
+
+		/**
+		* @brief Setup the AppTick event type and category in order to have some "introspection".
+		*/
 		EVENT_CLASS_TYPE(AppTick)
 		EVENT_CLASS_CATEGORY(EventCategoryApplication)
 	};
 
 	class AppUpdateEvent : public Event
 	{
+		/**
+		* @brief Constructor.
+		*/
 		AppUpdateEvent() {};
+
+		/**
+		* @brief Setup the AppUpdate event type and category in order to have some "introspection".
+		*/
 		EVENT_CLASS_TYPE(AppUpdate)
 		EVENT_CLASS_CATEGORY(EventCategoryApplication)
 	};
@@ -53,8 +86,14 @@ namespace MC
 	class AppRenderEvent : public Event
 	{
 	public:
+		/**
+		* @brief Constructor.
+		*/
 		AppRenderEvent() {};
 
+		/**
+		* @brief Setup the AppRender event type and category in order to have some "introspection".
+		*/
 		EVENT_CLASS_TYPE(AppRender)
 		EVENT_CLASS_CATEGORY(EventCategoryApplication)
 	};
--- MineCloneProject/src/MCP/Event/Event.h
+++ MineCloneProject/src/MCP/Event/Event.h
@@ -15,7 +15,10 @@ namespace MC
 		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
 	};
 
-
+	/**
+	* @brief Each EventCategory is shifted by N, in this way we can have multiple categories for an event using | or cheking
+	* if a event is in a certain category using &.
+	*/
 	enum EventCategory
 	{
 		None = 0,
@@ -26,7 +29,9 @@ namespace MC
 		EventCategoryMouseButton = BIT(4),
 	};
 
-	//Algum grau de introspecção
+	/**
+	* @brief Each subclass of Event will define/override its category and type, making possible some degree of introspection.
+	*/
 #define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
                                virtual EventType GetEventType() const override { return GetStaticType(); }\
                                virtual const char* GetName() const override { return #type; }
@@ -40,29 +45,56 @@ namespace MC
 	public:
 		bool Handled = false;
 
+		/**
+		* @brief Each subclass of Event will define/override its category and type, making possible some degree of introspection.
+		*/
 		virtual EventType GetEventType() const = 0;
 		virtual const char* GetName() const = 0;
 		virtual int GetCategoryFlags() const = 0;
 
+		/**
+		* @brief Allow us to print the type of the event and the info that it brings e.g: (WindowResize 1360, 720).
+		*/
 		virtual std::string ToString() const { return GetName(); }
 
+		/**
+		* @brief Each subclass of Event will define/override its category and type, making possible some degree of introspection.
+		* @param category - The event category to check if the event is part of. 
+		*/
 		inline bool IsInCategory(EventCategory category) { return GetCategoryFlags() & category; }
     };
 
 	class EventDispatcher
 	{
+		/**
+		* @brief Just a function pointer that returns a bool and takes a T.
+		* @param T - this have to be an Event, it will be used as argument for the function in the dispatcher.
+		*/
 		template<typename T>
 		using EventFn = std::function<bool(T&)>;
 
 	public:
+		/**
+		* @brief Constructor that takes an Event.
+		* @param Event that will be used to set m_Event.
+		*/
 		EventDispatcher(Event& event) : m_Event(event) {};
 
+		/**
+		* @brief This function takes an Event T as template argument and changes the argument's function pointer to take a T as argument.
+		* After checking if the T event type is the same as the m_Event, it calls the function with the casted event argument.
+		* The cast is made to a reference so we can assign a handled status to the event
+		*
+		* @param func - A function that returns bool and expects a certain type T of event.
+		* Example: dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(Application::OnWindowClose));
+		* in this case, OnWindowClose expects a WindowCloseEvent argument and returns a bool, so it can fit in the Dispatch requisite
+		*/
 		template<typename T>
 		bool Dispatch(EventFn<T> func)
 		{
 			if (m_Event.GetEventType() == T::GetStaticType())
 			{
-				m_Event.Handled = func(*(T*)&m_Event);
+				m_Event.Handled = func(static_cast<T&>m_Event);
 				return true;
 			}
 
