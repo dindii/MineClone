c80554f26cf08ab79d6f9323d31cb47d2a7ccda0
Dindi <didimoribeirojunior@hotmail.com>
2020-12-21 15:09:11 -0300

    Added ImGui in the project git as well as the premake5 file for an easy to build post cloning

---
+++ MineCloneProject/vendor/Imgui/premake5.lua
@@ -0,0 +1,38 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+	staticruntime "on"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
+
+    
+    filter "system:windows"
+        systemversion "latest"
+ 
+     defines
+	 {
+	 	 "MC_PLATFORM_WINDOWS"
+	 }
+ 
+     filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"             -- Deixamos on os debug symbols
+        
+     filter "configurations:Release"
+        runtime "Release"
+        optimize "on"            -- Ripamos todas as configurações de debug e otimizamos o projeto
